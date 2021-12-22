cf349d3eb14a76adf8a7edd1d95af9a7d4487980
NataliaShishonkova <lillyna@gmail.com>
2021-12-22 22:24:02 +0300

    homework 1

---
+++ src/main/java/course1/lesson1/FirstApp.java
@@ -0,0 +1,68 @@
+package course1.lesson1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        //System.out.println("Hello, java!");
+
+        int name = 10;
+
+        double doubleValue = 10.0;
+        int b = 100 + name;
+
+        //System.out.println("b = " + b + ";");
+        int c = name + b * 2;
+        //System.out.println(c);
+
+        int a = 42;
+        a++; // a = a + 1
+        a--; // a = a - 1
+        a += 10; // a = a+10
+        a -= 10; // a = a-10
+        a *= 10; // a = a*10
+        a /= 10; // a = a/10
+
+        boolean boolValue = true;
+        //System.out.println(boolValue);
+
+        int v1 = 10;
+        int v2 = 15;
+
+        if (v1 > v2) {
+            System.out.println("v1 > v2");
+        } else if (v1 < v2) {
+            System.out.println("v1 < v2");
+        } else {
+            System.out.println("v1 = v2");
+        }
+
+        drawCat();
+
+    }
+    // 2 типа данных:
+    // - ссылочные - живут в heap
+    // - примитивные - живут в стеке
+    // Численные типы
+    // byte -128 .. 127
+    // short 2^16 -2^15 .. 2^15-1
+    // int 2^32
+    // long 2^64
+
+    // Числа с плавающей точкой
+    // float 2^32
+    // double 2^64
+
+    // boolean
+
+    //Символьный
+    //char (ASCII/ Unicode) - беззнаковый short по размеру
+    public static void drawCat () {
+        System.out.println("           _..---...,\"\"-._     ,/}/)\n" +
+                "        .''        ,      ``..'(/-<\n" +
+                "       /   _      {      )         \\\n" +
+                "      ;   _ `.     `.   <         a(\n" +
+                "    ,'   ( \\  )      `.  \\ __.._ .: y\n" +
+                "   (  <\\_-) )'-.____...\\  `._   //-'\n" +
+                "    `. `-' /-._)))      `-._)))\n" +
+                "      `...'         ");
+    }
+}
