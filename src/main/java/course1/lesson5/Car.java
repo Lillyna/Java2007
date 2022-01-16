2858f52e656ec3bce2baab1ea69d60574d66d6a2
NataliaShishonkova <lillyna@gmail.com>
2022-01-16 22:18:13 +0300

    lesson5

--- src/main/java/course1/lesson5/Car.java
+++ src/main/java/course1/lesson5/Car.java
@@ -5,7 +5,77 @@ public class Car {
     public static int wheelsNumber=0;
 
     //fields
-    public String model;
-    public int year;
-    public boolean wasCrashed;
+    private String model;
+    private String modelFull;
+    private int year;
+    private boolean wasCrashed;
+
+    //Constructor
+    public Car(String model, int year, boolean wasCrashed){
+        this.model = model;
+        this.year = year;
+        this.wasCrashed = wasCrashed;
+    }
+    public Car(String model, int year){
+       this(model,year, false); // вызов другого конструктора самого себя
+    }
+    public Car(String model){
+        this(model, 2020);//вызовется 3 конструктора по очереди в конце концов
+    }
+
+    /**
+     * Выводит на экран информацию о машине
+     */
+    public void info(){
+        System.out.println("model = "+ model + ", year = "+ year + ", wasCrashed = "+ wasCrashed + ", wheelsNumber = "+ wheelsNumber);
+    }
+    public void printModel(){
+        System.out.println("model = "+ model);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public boolean isWasCrashed() {
+        return wasCrashed;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        if (year > 0) {
+            this.year = year;
+        }
+        else{
+            System.out.println("Error");
+        }
+    }
+    public void setWasCrashed(boolean wasCrashed) {
+        this.wasCrashed = wasCrashed;
+    }
+
+    /**
+     * Попала в аварию
+     */
+    public void crash(){
+        System.out.println("Car was crashed");
+        wasCrashed = true;
+    }
+    public void repair(){
+        if(wasCrashed){
+            wasCrashed = false;
+            System.out.println("Repaired");
+        }
+        else{
+            System.out.println("Nothing to repair");
+        }
+    }
 }
--- src/main/java/course1/lesson5/DemoApp.java
+++ src/main/java/course1/lesson5/DemoApp.java
@@ -2,20 +2,19 @@ package course1.lesson5;
 
 public class DemoApp {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.model = "Dodge";
-        car.year = 2020;
-        car.wheelsNumber = 4;
+        Car car = new Car("Dodge", 2020, false);
 
-        System.out.println("model = "+ car.model + ", year = "+ car.year + ", wasCrashed = "+ car.wasCrashed + ", wheelsNumber = "+ car.wheelsNumber);
+        car.info();
+        car.crash();
+        car.info();
 
-        Car car2 = new Car();
-        car2.model = "Ford";
-        car2.year = 1990;
-        car2.wheelsNumber = 6;
 
-        System.out.println("model = "+ car.model + ", year = "+ car.year + ", wasCrashed = "+ car.wasCrashed + ", wheelsNumber = "+ car.wheelsNumber);
-        System.out.println("model = "+ car2.model + ", year = "+ car2.year + ", wasCrashed = "+ car2.wasCrashed + ", wheelsNumber = "+ car2.wheelsNumber);
+        Car car2 = new Car("Ford", 1990);
+
+        car2.printModel();
+        String carModel = car2.getModel();
+        System.out.println("model = "+ car.getModel() + ", year = "+ car.getYear() + ", wasCrashed = "+ car.isWasCrashed() + ", wheelsNumber = "+ car.wheelsNumber);
+        System.out.println("model = "+ car2.getModel() + ", year = "+ car2.getYear() + ", wasCrashed = "+ car2.isWasCrashed() + ", wheelsNumber = "+ car2.wheelsNumber);
 
         int wheelNumer = Car.wheelsNumber;// можно запросить сразу у класса, а не у объекта.
         Car[] cars = new Car[]{car, car2};
