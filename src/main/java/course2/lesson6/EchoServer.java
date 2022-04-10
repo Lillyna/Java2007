925ec76ee02f219a0d48776683a4e1b132dce211
Lillyna <96077239+Lillyna@users.noreply.github.com>
2022-04-10 03:03:03 +0300

    homework 6

--- src/main/java/course2/lesson6/EchoServer.java
+++ src/main/java/course2/lesson6/EchoServer.java
@@ -1,38 +1,64 @@
 package course2.lesson6;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
 
     public static void main(String[] args) {
-        Socket socket = null;
+        EchoServer echoServer = new EchoServer();
+        echoServer.start();
+    }
 
-        try (ServerSocket serverSocket = new ServerSocket(8189)//;
-             // ServerSocket serverSocket1 = new ServerSocket(8190)
-        )
-        { //try with resources
+    private void start() {
+        try (ServerSocket serverSocket = new ServerSocket(8189)
+        ) { //try with resources
             System.out.println("Сервер запущен, ожидаем подключение");
-            socket = serverSocket.accept();// блокирующий метод, блокирует пока не подключится какой-нибудь клиент
+            Socket socket = serverSocket.accept();// блокирующий метод, блокирует пока не подключится какой-нибудь клиент
             System.out.println("Клиент подключился");
             DataInputStream in = new DataInputStream(socket.getInputStream());//получение сообщения
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());//отправление сообщения
-            while (true){
-                String message = in.readUTF();//блокирующий метод
-                if("/end".equalsIgnoreCase(message)){
-                    out.writeUTF("/end");
-                    break;
-                }
-                out.writeUTF("echo " + message);
-            }
+            sendMsgToClient(socket, out);
+            listenMsgFromClient(in, out);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("Сервер остановлен");
+            System.exit(0);
         }
-        System.out.println("Сервер остановлен");
+    }
+
+    private void sendMsgToClient(Socket socket, DataOutputStream out) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Scanner scanner = new Scanner(System.in);
+                    while (true) {
+                        String s = scanner.nextLine();
+                        System.out.println("Посылаю на клиент сообщение: " + s);
+                        out.writeUTF(s);// посылка сообщений на клиент
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    private void listenMsgFromClient(DataInputStream in, DataOutputStream out) throws IOException {
+        while (true) {
+            String message = in.readUTF();//блокирующий метод
+            if ("/end".equalsIgnoreCase(message)) {
+                out.writeUTF("/end");
+                break;
+            }
+            out.writeUTF("echo " + message);
+        }
     }
 }
