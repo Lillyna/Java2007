71b8abc993b1e04081b003e2d51447417089dfd4
Lillyna <96077239+Lillyna@users.noreply.github.com>
2022-05-21 00:15:41 +0300

    homework6-course3 part2

--- src/test/java/course3/homework6/CheckArrayTest.java
+++ src/test/java/course3/homework6/CheckArrayTest.java
@@ -2,9 +2,9 @@ package course3.homework6;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -19,21 +19,30 @@ public class CheckArrayTest {
     public void init() {
         checkArray = new CheckArray();
     }
-
-    private static Stream<Arguments> provideArgs() {
+    private static Stream<Arguments> providePositiveTestingArgs(){
         return Stream.of(
-                Arguments.of(new Integer[]{1, 2, 3, 4, 5, 6, 7, 4, 1, 2}), //have 1 & 4 several times
-                Arguments.of(new Integer[]{0, 2, 3, 4, 5, 6}),             // only 4 once
-                Arguments.of(new Integer[]{-8, 2, 3, 1, 5, 6}),            //only 1 once
-                Arguments.of(new Integer[]{1, 1, 1, 1, 1, 1})              //only 1 all in array
-
+                Arguments.of((Object) new Integer[]{1, 2, 3, 4, 5, 6, 7, 4, 1, 2}), //have 1s & 4s
+                Arguments.of((Object) new Integer[]{0, 2, 3, 4, 5, 6}), // have only 4 once
+                Arguments.of((Object) new Integer[]{-8, 2, 3, 1, 5, 6}), // have only 1 once
+                Arguments.of((Object) new Integer[]{1, 1, 1, 1, 1, 1}) //have 1s for every element
+        );
+    }
+    private static Stream<Arguments> provideNegativeTestingArgs(){
+        return Stream.of(
+                Arguments.of((Object) new Integer[]{9, 2, 3, 6, 5, 6, 7, 7, 3, 2}), //no 4s nor 1s
+                Arguments.of((Object) new Integer[]{}) // empty array
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("providePositiveTestingArgs")
+    public void testPosSubArray(Integer[] inputArray) {
+        Assertions.assertTrue(checkArray.checkArray(inputArray));
+    }
 
     @ParameterizedTest
-    @MethodSource("provideArgs")
-    public void testSubArray(Integer[] inputArray) {
-        Assertions.assertTrue(checkArray.checkArray(inputArray));
+    @MethodSource("provideNegativeTestingArgs")
+    public void testNegSubArray(Integer[] inputArray) {
+        Assertions.assertFalse(checkArray.checkArray(inputArray));
     }
 }
